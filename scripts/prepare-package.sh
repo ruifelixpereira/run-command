87feab4d6cd90fcf356ed25cdfda4cb5ac550eeb
v1user <rfp@rfp-studio>
2023-10-16 21:34:54 +0100

    fix release

--- scripts/prepare-package.sh
+++ scripts/prepare-package.sh
@@ -6,6 +6,7 @@ npm run prestart
 
 REVISION=$(jq -r .version package.json)
 
+scripts/prepare-package.sh
 zip -r ./packages/runcmd-func-app-rev${REVISION}.zip . -x ./docs/**\* ./src/**\* ./scripts/**\* ./.git/**\* ./.vscode/**\* ./packages/**\* MYLOG.md local.settings.json
 
 cd $currentDir
---
+++ scripts/tag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+currentDir=$(pwd)
+cd ..
+REVISION=$(jq -r .version package.json)
+
+git tag -a v${REVISION} -m "Tagging version ${REVISION}"
+
+git push origin --tags
+
+cd $currentDir
